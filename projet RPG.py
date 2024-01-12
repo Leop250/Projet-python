9fc6de5cfc61116de8df1ca6f6556c1da0e6266e
Leop250 <154471229+Leop250@users.noreply.github.com>
2024-01-12 17:49:07 +0100

    Update projet RPG.py

--- projet RPG.py
+++ projet RPG.py
@@ -21,7 +21,7 @@ def start():
     action = input()
     if action == "1":
         print("La partie ce lance")
-        print("Dans un royaume lointain vivait un soldat fan d'un rappeur: Sefyu. Alors agacé d'entendre en boucle J'peux manger du cassoulet halal en même temps que le mafé il ce fit bannir dans une forêt gardé par un être puissant que peux connaisait")
+        print("Dans un royaume lointain vivait un soldat fan d'un rappeur: Sefyu. Alors agacé d'entendre en boucle J'peux manger du cassoulet halal en même temps que le mafé. Le roi le  fit bannir dans une forêt gardé par un être puissant que peux connaisait")
         print("Allez vous réussir à  sortir du bois d'Arcy?")
         print("il est temps de choisir le type de personage que tu souhaites: ")
         print("Guerrier avec 20 de force,  20 de defense et 100 hp ")
@@ -131,31 +131,12 @@ class PlayerRPG:
             self.y = game_state['y']
             self.inventory = game_state['inventory']
 
-    def afficher_statut_joueur(self):
-        print("\033[93mSanté : {}\033[0m".format(self.HP))
-
-    def afficher_carte(self):
-        for i, ligne in enumerate(self.game_map):
-            for j, tuile in enumerate(ligne):
-                if (i, j) == (self.x, self.y):
-                    print("\033[93m{}\033[0m".format(tuile), end=" ")
-                elif tuile == "gobelin":
-                    print("\033[91m{}\033[0m".format(tuile), end=" ")
-                elif tuile == "ogre":
-                    print("\033[91m{}\033[0m".format(tuile), end=" ")
-                elif tuile == "dragon":
-                    print("\033[91m{}\033[0m".format(tuile), end=" ")
-                elif(i, j) == (self.x, self.y):
-                    print("\033[93mJ\033[0m", end=" ")
-                else:
-                    print(tuile, end=" ")
-            print()
-
     def map_init(self):
         list_obj = []
 
         for coffre in range(0, 10):
             list_obj.append("coffre")
+
         for monstre_facile in range(0, 10):
             list_obj.append("goblin")
 
@@ -302,70 +283,69 @@ class PlayerRPG:
                 return (self.classe, self.strength, self.defense, self.HP, self.XP)
 
     def combat(self,monster):
-        reward = ["potion force", "potion regen", "potion defense"]
-        monster_name = monster
-        monster = monsters.get(monster)
-        hp_memory = monster["HP"]
-        soudoyer = input("voulez-vous tenter de soudoyer le monstre: oui ou non: ")
-        if soudoyer not in ["oui", "non"]:
-            while soudoyer not in ["oui", "non"]:
-                print("Veuillez répondre par 'oui' ou 'non'.")
-                soudoyer = input("Voulez-vous tenter de soudoyer le monstre ? oui ou non")
-            
-        soudoyer = soudoyer.lower()
+            reward = ["potion force", "potion regen", "potion defense"]
+            monster_name = monster
+            monster = monsters.get(monster)
+            hp_memory = monster["HP"]
+            soudoyer = input("voulez-vous tenter de soudoyer le monstre: oui ou non: ")
+            if soudoyer not in ["oui", "non"]:
+                while soudoyer not in ["oui", "non"]:
+                    print("Veuillez répondre par 'oui' ou 'non'.")
+                    soudoyer = input("Voulez-vous tenter de soudoyer le monstre ? oui ou non")
 
-        if self.inventory["PO"] < monster["soudoyer"] and soudoyer == "oui":
-            print("dommage vous n'avez pas assez d'or sur vous")
-            print(f"il vous manque {monster['soudoyer'] - self.inventory['PO']}PO")
-        if self.inventory["PO"] < monster["soudoyer"] or soudoyer != "oui":
-            while self.HP > 0 and monster["HP"] > 0:
-                atk = input(f"choice an attack {list(self.attack.keys())}")
+            soudoyer = soudoyer.lower()
 
-                while atk not in (self.attack.keys()):
-                    print(f"Choisissez une attaque parmi {list(self.attack.keys())}")
-                    atk = input(f"Choisissez une attaque parmi {list(self.attack.keys())}")
-                atk = atk.lower()
-                self.inventory_use()
-                accuracy = random.randint(0,100)
-                if accuracy <= self.attack[atk][1]:
-                    monster["HP"] -= (self.strength + self.attack[atk][0]) - monster["defense"]
-                    print(f"vous utiliser {atk}  {monster_name} PV = {monster['HP']}")
-                    monster_atk = monster["attack"]
-                    if monster_atk <= self.defense and monster["HP"] > 0 :
-                        print("votre defense est trop élevé l'attaque du monstre ne vous fait aucun dégats")
-                    elif monster["HP"] > 0:
-                        self.HP -= monster_atk + self.defense
-                        print(f"le monstre vous inflige {monster_atk} de dommage, il vous reste {self.HP} PV")
-                else:
-                    print("vous ratez votre attaque")
-                    monster_atk = monster["attack"]
-                    if monster_atk <= self.defense:
-                        print("votre defense est trop élevé l'attaque du monstre ne vous fait aucun dégats")
+            if self.inventory["PO"] < monster["soudoyer"] and soudoyer == "oui":
+                print("dommage vous n'avez pas assez d'or sur vous")
+                print(f"il vous manque {monster['soudoyer'] - self.inventory['PO']}PO")
+            if self.inventory["PO"] < monster["soudoyer"] or soudoyer != "oui":
+                while self.HP > 0 and monster["HP"] > 0:
+                    atk = input(f"choice an attack {list(self.attack.keys())}")
 
+                    while atk not in (self.attack.keys()):
+                        print(f"Choisissez une attaque parmi {list(self.attack.keys())}")
+                        atk = input(f"Choisissez une attaque parmi {list(self.attack.keys())}")
+                    atk = atk.lower()
+                    self.inventory_use()
+                    accuracy = random.randint(0,100)
+                    if accuracy <= self.attack[atk][1]:
+                        monster["HP"] -= (self.strength + self.attack[atk][0]) - monster["defense"]
+                        print(f"vous utiliser {atk}  {monster_name} PV = {monster['HP']}")
+                        monster_atk = monster["attack"]
+                        if monster_atk <= self.defense and monster["HP"] > 0 :
+                            print("votre defense est trop élevé l'attaque du monstre ne vous fait aucun dégats")
+                        elif monster["HP"] > 0:
+                            self.HP -= monster_atk + self.defense
+                            print(f"le monstre vous inflige {monster_atk} de dommage, il vous reste {self.HP} PV")
                     else:
-                        self.HP =  self.HP - monster_atk + self.defense
-                        print(f"le monstre vous inflige {monster_atk - self.defense} de dommage, il vous reste {self.HP} PV")
-            if monster["HP"] <= 0 :
-                self.XP += monster["XP"]
-                monster["HP"] = hp_memory
-                print (f"vous avez vaincu le {monster_name} ,vous avez gagné {monster['XP']}xp, vous obtenez {random.choice(reward)}")
+                        print("vous ratez votre attaque")
+                        monster_atk = monster["attack"]
+                        if monster_atk <= self.defense:
+                            print("votre defense est trop élevé l'attaque du monstre ne vous fait aucun dégats")
 
-            elif self.HP <= 0:
-                print("LOOSER TU AS PERDU LOOOOOSER")
+                        else:
+                            self.HP =  self.HP - monster_atk + self.defense
+                            print(f"le monstre vous inflige {monster_atk - self.defense} de dommage, il vous reste {self.HP} PV")
+                if monster["HP"] <= 0 :
+                    self.XP += monster["XP"]
+                    monster["HP"] = hp_memory
+                    print (f"vous avez vaincu le {monster_name} ,vous avez gagné {monster['XP']}xp, vous obtenez {random.choice(reward)}")
 
-        elif self.inventory["PO"] >= monster["soudoyer"] and soudoyer == "oui":
-            self.inventory["PO"] -= monster["soudoyer"]
-            print("haha les bons comptes font les bons amis, ça va pour cette fois passe ton chemin, vous donner {} PO à {},\n il vous reste {} PO".format(monster["soudoyer"], monster_name, self.inventory["PO"]))
+                elif self.HP <= 0:
+                    print("TU AS PERDU !!!!!!")
 
+            elif self.inventory["PO"] >= monster["soudoyer"] and soudoyer == "oui":
+                self.inventory["PO"] -= monster["soudoyer"]
+                print("haha les bons comptes font les bons amis, ça va pour cette fois passe ton chemin, vous donner {} PO à {},\n il vous reste {} PO".format(monster["soudoyer"], monster_name, self.inventory["PO"]))
 
 
     def event(self):
-
+        visited = ["Je jure je suis déja passé par la", "Paris c'est loin", "Je sortirai d'ici pour ceux qui mange pas de porc"]
         position = self.perform_move()
-        
+        print("Current position:", position)
 
         if position in self.visited_positions:
-            print("Vous êtes déjà passé par ici. Aucun événement en conséquence.")
+            print(random.choices(visited))
             return
         else:
             self.visited_positions.add(position)
@@ -383,21 +363,27 @@ class PlayerRPG:
             if objet_win in self.inventory:
                 self.inventory[objet_win] += 1
                 print("bravo vous avez gagné {} votre inventaire est maintenant de {}".format(objet_win, self.inventory))
+
             else:
                 self.inventory[objet_win] = 1
                 print("bravo vous avez gagné {} votre inventaire est maintenant de {}".format(objet_win, self.inventory))
+
         elif position == "marchand":
+            print("Besoin de quelque chose? J'ai tous, de l'illégal aussi!!")
             self.buy_merchant()
         elif position == "goblin":
+            print("Une tête chauve un et nez pointu comme une fléchette sortent d'un buisson")
             self.combat("gobelin")
 
 
         elif position == "ogre":
+            print("Des pas lourds et une jupe fleurit, un frison parcours l'échine")
             self.combat("ogre")
 
 
 
         elif position == "dragon":
+            print("Un lézard qui crache le feu comme un dragon est en face")
             self.combat("dragon")
 
 
@@ -417,8 +403,9 @@ class PlayerRPG:
 
         elif position == "voleur de divers":
             cle_aleatoire = random.choice(list(self.inventory.keys()))
+            print("T'as plus rien maintenant bouffon")
 
-            if self.inventory[cle_aleatoire] > 0: #va faire mois 10pv si l objet aleatoire est pas dans l invent
+            if self.inventory[cle_aleatoire] > 0:
                 self.inventory[cle_aleatoire] -= 1
                 print("le voleur vous a pris {}".format(cle_aleatoire))
             else:
@@ -446,7 +433,7 @@ class PlayerRPG:
             self.inventory["potion_defense"] += 1
             print("Vous avez maintenant {} potion de defense et {} PO".format(self.inventory["potion_defense"], self.inventory["PO"]))
         elif choice == "4":
-          print("Si mes articles sont guez tu peux dégager")
+          print("Si mes articles sont pas bon tu peux dégager")
         else:
             print("Error")
 
@@ -608,5 +595,25 @@ class PlayerRPG:
         }
         save_game(actual_state)
         print("Le jeu a été sauvegardé.")
+
+    def afficher_statut_joueur(self):
+        print("\033[92m Santé : {}\033[92m ".format(self.HP))
+
+    def afficher_carte(self):
+        for i, ligne in enumerate(self.game_map):
+            for j, tuile in enumerate(ligne):
+                if (i, j) == (self.x, self.y):
+                    print("\033[93m{}\033[0m".format(tuile), end=" ")
+                elif tuile == "gobelin":
+                    print("\033[91m{}\033[0m".format(tuile), end=" ")
+                elif tuile == "ogre":
+                    print("\033[91m{}\033[0m".format(tuile), end=" ")
+                elif tuile == "dragon":
+                    print("\033[91m{}\033[0m".format(tuile), end=" ")
+                elif(i, j) == (self.x, self.y):
+                    print("\033[93mJ\033[0m", end=" ")
+                else:
+                    print(tuile, end=" ")
+            print()
 
 start()
