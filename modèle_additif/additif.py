eb376bec6cfc2c6b92c0e86b7edc711444693d7b
Leop250 <154471229+Leop250@users.noreply.github.com>
2024-01-31 17:06:13 +0100

    Update additif.py

--- modèle_additif/additif.py
+++ modèle_additif/additif.py
@@ -5,61 +5,49 @@ import pandas as pd
 import seaborn as sns
 import statsmodels.api as sm
 from math import *
-from scipy.stats import t
 
+class DataPreparation:
+    def __init__(self, dataset_df):
+        self.dataset_df = dataset_df
+        """
+        Cette classe prend en entrée un chemin de fichier csv.
+        Elle split le jeu de donnée en 2 bases 
+        + une train 75 %
+        + une test 25 %
+        Ce 2 bases, la classe va les splits en 2 
 
-class Additif:
-    def __init__(self, data_preparation_object):
-        self.data_preparation_object = data_preparation_object
-        self.model = LinearRegression()
+        + un vecteur x (qui contient les indexs temporels)
+        + un vecteur y (qui contient les valeurs à prédire)
+        En tout cette va extraire 4 arrays.
+        x_train
+        y_train
+        x_test
+        y_test
+        """
 
-        self.model.fit(data_preparation_object.x_train, data_preparation_object.y_train)
+        self.dataset_df = pd.read_csv('vente_maillots_de_bain.csv', encoding='ISO-8859-1')
 
-        y_train_predicted = self.model.predict(data_preparation_object.x_train)
-        mean_train_absolute_error = np.abs(np.mean(y_train_predicted - data_preparation_object.y_train))
-        print(f"sur le jeu de train : {mean_train_absolute_error=:.2f}")
+        self.dataset_df['Years'] = pd.to_datetime(self.dataset_df['Years'])
+        self.dataset_df['month_name'] = self.dataset_df['Years'].dt.strftime('%B')
 
-        y_test_predicted = self.model.predict(data_preparation_object.x_test)
-        mean_test_absolute_error = np.mean(np.abs(y_test_predicted - data_preparation_object.y_test))
-        print(f"sur le jeu de test : {mean_test_absolute_error=:.2f}")
+        self.dataset_df = pd.get_dummies(self.dataset_df, columns=['month_name'], drop_first=True)
+        self.dataset_df.replace({True: 1, False: 0}, inplace=True)
+        self.dataset_df['index_interation'] = np.arange(1, len(self.dataset_df) + 1)
 
-        self.show_model_predictions(y_train_predicted, y_test_predicted)
+        self.dataset_df = pd.DataFrame(self.dataset_df)
+        self.prepare_data()
+        dataset_df
+    def prepare_data(self):
+        number_of_rows = len(self.dataset_df)
+        self.dataset_df["month_name"] = np.arange(0, number_of_rows, 1)
 
+        dataset_train_df = self.dataset_df.iloc[:int(number_of_rows * 0.75)]
+        dataset_test_df = self.dataset_df.iloc[int(number_of_rows * 0.75):]
 
-    
+        self.x_train = dataset_train_df.iloc[:, 3:14].values
+        #sous dataframe
+        self.y_train = dataset_train_df[['Sales']].values
 
-    def show_model_predictions(self, y_train_predicted, y_test_predicted):
-        conf = 0.95
-
-       
-        residuals = y_test_predicted - self.data_preparation_object.y_test
-        std_residuals = np.std(residuals)
-        n = len(y_test_predicted)
-        interval = 1.96 * (std_residuals / np.sqrt(n))  # 1.96 pour un intervalle de confiance de 95%
-
-        min_interval = (y_test_predicted - interval).ravel()
-        max_interval = (y_test_predicted + interval).ravel()
-
-        
-        plt.figure(figsize=(15, 6))
-        plt.plot(
-            self.data_preparation_object.dataset_df['Years'][:len(self.data_preparation_object.x_train)],
-            self.data_preparation_object.y_train, "bo-", label='Actual Train Sales')
-        plt.plot(
-            self.data_preparation_object.dataset_df['Years'][:len(self.data_preparation_object.x_train)],
-            y_train_predicted, "b-", label='Predicted Train Sales')
-
-        plt.plot(
-            self.data_preparation_object.dataset_df['Years'][len(self.data_preparation_object.x_train):],
-            self.data_preparation_object.y_test, "ro-", label='Actual Test Sales')
-        plt.plot(
-            self.data_preparation_object.dataset_df['Years'][len(self.data_preparation_object.x_train):],
-            y_test_predicted, "r-", label='Predicted Test Sales')
-
-     
-        plt.fill_between(self.data_preparation_object.dataset_df['Years'][len(self.data_preparation_object.x_train):],
-                         min_interval, max_interval,
-                         color="lightgray", alpha=0.5, label="95% Confidence Interval")
-
-        plt.legend()
-        plt.show()
+        self.x_test = dataset_test_df.iloc[:, 3:14].values
+        #sous dataframe on obtien un array d'array2
+        self.y_test = dataset_test_df[['Sales']].values
